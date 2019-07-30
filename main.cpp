02ff3ddc06ea38d4a427ac1abc9f112dfb6d46ce
Gennadiy <gennadiy1g@gmx.com>
2019-07-30 10:54:13 -0400

    WIP on main.

--- main.cpp
+++ main.cpp
@@ -8,23 +8,27 @@ int main(int argc, char** argv)
 {
     try {
         bpo::options_description hiddenOptions("Hidden options");
-        hiddenOptions.add_options()("input-file", bpo::value<std::string>(), "input file")("output-table", bpo::value<std::string>(), "input file");
+        hiddenOptions.add_options()("file-name", bpo::value<std::string>(), "file name")("table-name", bpo::value<std::string>(), "table name");
 
         bpo::options_description visibleOptions("Allowed options");
-        visibleOptions.add_options()("help,?", "print usage information and exit")("separator,S", "field separator character")("quote,Q", "string quote character")("driver,D", "ODBC driver name (default MonetDB ODBC Driver)")("host,H", "name of the host on which the server runs (default: localhost)")("port,P", "portnumber of the server (default: 50000)")("uid,U", "user to connect as (default monetdb)")("pwd,P", "user's password (default monetdb)");
+        visibleOptions.add_options()("help,?", "Print usage information and exit.");
+        visibleOptions.add_options()("separator,S", "Field separator character.")("quote,Q", "String quote character.");
+        visibleOptions.add_options()("driver,D", "ODBC driver name (default MonetDB ODBC Driver)");
+        visibleOptions.add_options()("host,H", "name of the host on which the server runs (default: localhost)")("port,P", "portnumber of the server (default: 50000)");
+        visibleOptions.add_options()("uid,U", "user to connect as (default monetdb)")("pwd,P", "user's password (default monetdb)");
 
         bpo::options_description commandLineOptions;
         commandLineOptions.add(visibleOptions).add(hiddenOptions);
 
         bpo::positional_options_description positionalOptions;
-        positionalOptions.add("input-file", -1);
+        positionalOptions.add("file-name", 0).add("table-name", 1);
 
         bpo::variables_map variablesMap;
         store(bpo::command_line_parser(argc, argv).options(commandLineOptions).positional(positionalOptions).run(), variablesMap);
         notify(variablesMap);
 
         if (variablesMap.count("help")) {
-            std::cout << visibleOptions << "\n";
+            std::cout << visibleOptions << std::endl;
             return 0;
         }
     } catch (const std::exception& e) {
