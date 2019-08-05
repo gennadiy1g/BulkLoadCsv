df52a7306549178740d001f9b9af03231282c547
Gennadiy <gennadiy1g@gmx.com>
2019-08-05 17:24:38 -0400

    Added more of debugging output.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ extern "C" int wmain(int argc, wchar_t** argv)
 
         // Running Examples under Microsoft Windows
         // https://www.boost.org/doc/libs/1_69_0/libs/locale/doc/html/running_examples_under_windows.html
-        std::wcout << argc << ' ' << argv[0] << ' ' << (argc >= 2 ? argv[1] : L"") << std::endl;
+        std::wcout << argc << ' ' << argv[0] << ' ' << (argc >= 2 ? argv[1] : L"") << ' ' << (argc >= 3 ? argv[2] : L"") << std::endl;
 
         bpo::options_description hiddenOptions("Hidden options");
         hiddenOptions.add_options() //
