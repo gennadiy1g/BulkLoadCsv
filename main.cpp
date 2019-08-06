2c74d9a7522c0ccfbfad4ef426092eeb09324f7c
Gennadiy <gennadiy1g@gmx.com>
2019-08-06 18:15:03 -0400

    Added handling program options port, uid, pwd.

--- main.cpp
+++ main.cpp
@@ -37,8 +37,8 @@ extern "C" int wmain(int argc, wchar_t** argv)
             ("separator,S", bpo::wvalue<std::wstring>()->default_value(L",", ","), "Field separator character.") //
             ("quote,Q", bpo::wvalue<std::wstring>()->default_value(L"\"", "\""), "String quote character.") //
             ("port,P", bpo::value<int>()->default_value(50000), "Port number of the server.") //
-            ("uid,U", bpo::value<std::string>()->default_value("monetdb"), "User to connect as.") //
-            ("pwd", bpo::value<std::string>()->default_value("monetdb"), "Password.") //
+            ("uid,U", bpo::wvalue<std::wstring>()->default_value(L"monetdb", "monetdb"), "User to connect as.") //
+            ("pwd", bpo::wvalue<std::wstring>()->default_value(L"monetdb", "monetdb"), "Password.") //
             ("print-sql", "Scan the file and print the generated SQL commands before executing them.") //
             ("dry-run,D", "Scan the file and print the generated SQL commands but do not execute them.");
 
@@ -63,6 +63,21 @@ extern "C" int wmain(int argc, wchar_t** argv)
         } else {
             std::wcout << "Scanning " << variablesMap["file-name"].as<std::wstring>() << std::endl;
             MonetDBBulkLoader bulkLoader(variablesMap["file-name"].as<std::wstring>());
+            if (variablesMap.count("port") && !variablesMap["port"].defaulted()) {
+                std::vector<ConnectionParameter> connectionParameters;
+                connectionParameters.push_back(std::make_pair(L"PORT", std::to_wstring(variablesMap["port"].as<int>())));
+                bulkLoader.setConnectionParameters(connectionParameters);
+            }
+            if (variablesMap.count("uid") && !variablesMap["uid"].defaulted()) {
+                std::vector<ConnectionParameter> connectionParameters;
+                connectionParameters.push_back(std::make_pair(L"UID", variablesMap["uid"].as<std::wstring>()));
+                bulkLoader.setConnectionParameters(connectionParameters);
+            }
+            if (variablesMap.count("pwd") && !variablesMap["pwd"].defaulted()) {
+                std::vector<ConnectionParameter> connectionParameters;
+                connectionParameters.push_back(std::make_pair(L"PWD", variablesMap["pwd"].as<std::wstring>()));
+                bulkLoader.setConnectionParameters(connectionParameters);
+            }
             bulkLoader.parse(variablesMap["separator"].as<std::wstring>()[0], variablesMap["quote"].as<std::wstring>()[0]);
             auto rejectedRecords = bulkLoader.load(variablesMap["table-name"].as<std::wstring>());
             if (rejectedRecords.value_or(0) > 0) {
